3dfaaa780d3c104e3db3da90e44395c9f02790bb
Florian Rampazzi <florian.rampazzi@gmail.com>
2022-09-22 13:02:14 +0200

    Adding scripts for products :  getting API's content | display one product in HTML | loop for each products from API

---
+++ front/js/script.js
@@ -0,0 +1,25 @@
+//Récupération de la liste des produits depuis l'API.
+fetch("http://localhost:3000/api/products")
+    .then(kanapRes => kanapRes.json())
+    .then(kanaps => displayKanaps(kanaps))
+    .catch("Aucun produit disponible")
+    
+    //Fonction qui permet d'afficher un produit.
+    const displayOneKanap = (kanap) => {
+
+      //Injection litterale en HTML d'un produit.
+      const nodeKanap = `<a href="./product.html?id=${kanap._id}">
+      <article>
+        <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
+        <h3 class="productName">${kanap.name}</h3>
+        <p class="productDescription">${kanap.description}</p>
+      </article>
+      </a>`;
+
+      document.getElementById('items').innerHTML += nodeKanap;
+    }
+
+    // Fonction qui permet d'afficher tous les produits un par un avec une boucle forEach.
+    function displayKanaps(kanaps) {
+      kanaps.forEach(kanap => displayOneKanap(kanap))
+    }
