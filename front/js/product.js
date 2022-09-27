7bda3d0b99ae2e008e5ac1c700a784eebb020e2e
Florian Rampazzi <florian.rampazzi@gmail.com>
2022-09-27 16:13:29 +0200

    Adding product.js : display productinformations from id | adding product to cart and localStorage

---
+++ front/js/product.js
@@ -0,0 +1,66 @@
+let url = new URL(window.location.href);
+let productId = url.searchParams.get("id");
+
+fetch(`http://localhost:3000/api/products/${productId}`)
+    .then(kanapInfoRes => kanapInfoRes.json())
+    .then(kanapInfo => displayKanapInfo(kanapInfo))
+    .catch(error => error.console.log("Erreur de récupération de l'API produits"))
+
+// Affichage des informations d'un canapé
+const displayKanapInfo = (kanapInfo) => {
+    document.getElementsByClassName('item__img')[0].innerHTML = `<img src="${kanapInfo.imageUrl}" alt="${kanapInfo.altTxt}"></img>`;
+    document.getElementById('title').innerHTML = kanapInfo.name;
+    document.getElementById('price').innerHTML = kanapInfo.price;
+    document.getElementById('description').innerHTML = kanapInfo.description;
+
+    // Parcours du Array color et affichage de chacune
+    kanapInfo.colors.forEach(
+        color => document.getElementById('colors').innerHTML += `<option value="${color}">${color}</option>`
+    )
+}
+
+let cart = [];
+
+document.getElementById('addToCart').addEventListener ('click', () => {
+    let selectedColor = document.getElementById("colors");
+    let productColor = selectedColor.options[selectedColor.selectedIndex].text;
+    let productQuantity = Number(document.getElementById("quantity").value);
+    let isInCart = false;
+
+    const kanap = {
+        id: productId,
+        color: productColor,
+        quantity: productQuantity
+    };
+
+    cart.forEach(cartProduct => {
+        if (compareKanap(productId, cartProduct, productColor)) {
+            cartProduct.quantity = productQuantity + Number(cartProduct.quantity);
+            isInCart = true;
+        } 
+    });
+
+    if (isInCart == false) {
+        cart.push(kanap);
+    }
+
+    // cart.forEach(cartProduct => {
+    //     if (productId == cartProduct[0] && productColor == cartProduct[1]) {
+    //         cartProduct[2] = productQuantity + Number(cartProduct[2]);
+    //         isInCart = true;
+    //     } 
+    // });
+
+    // if (isInCart == false) {
+    //     cart.push([`${productId}`,`${productColor}`,`${productQuantity}`]);
+    // }      
+
+    let kanapOrder = JSON.stringify(cart);
+    localStorage.setItem("kanap Order",kanapOrder);
+});
+
+function compareKanap(productId, kanap, productColor){
+    return productId == kanap.id && productColor == kanap.color
+}
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -2,24 +2,22 @@
 fetch("http://localhost:3000/api/products")
     .then(kanapRes => kanapRes.json())
     .then(kanaps => displayKanaps(kanaps))
-    .catch("Aucun produit disponible")
+    .catch(error => error.console.log("Erreur de récupération de l'API produits"))
     
-    //Fonction qui permet d'afficher un produit.
-    const displayOneKanap = (kanap) => {
+//Fonction qui permet d'afficher un produit.
+const displayOneKanap = (kanap) => {
+  //Injection litterale en HTML d'un produit.
+  const nodeKanap = `<a href="./product.html?id=${kanap._id}">
+  <article>
+    <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
+    <h3 class="productName">${kanap.name}</h3>
+    <p class="productDescription">${kanap.description}</p>
+  </article>
+  </a>`;
+  document.getElementById('items').innerHTML += nodeKanap;
+}
 
-      //Injection litterale en HTML d'un produit.
-      const nodeKanap = `<a href="./product.html?id=${kanap._id}">
-      <article>
-        <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
-        <h3 class="productName">${kanap.name}</h3>
-        <p class="productDescription">${kanap.description}</p>
-      </article>
-      </a>`;
-
-      document.getElementById('items').innerHTML += nodeKanap;
-    }
-
-    // Fonction qui permet d'afficher tous les produits un par un avec une boucle forEach.
-    function displayKanaps(kanaps) {
-      kanaps.forEach(kanap => displayOneKanap(kanap))
-    }
+// Fonction qui permet d'afficher tous les produits un par un avec une boucle forEach.
+function displayKanaps(kanaps) {
+  kanaps.forEach(kanap => displayOneKanap(kanap))
+}
